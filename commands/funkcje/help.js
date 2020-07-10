75178d3b391528fa568edec969b67755de8eea9e
thugsofindia <58879584+THUGSOFINDIA@users.noreply.github.com>
2020-07-10 08:26:24 +0530

    Update help.js

--- commands/funkcje/help.js
+++ commands/funkcje/help.js
@@ -12,18 +12,18 @@ class helpCommand extends commando.Command {
     }
     async run(message, args) {
         let embed = new Discord.RichEmbed()
-            .setTitle('**Lista dostępnych komend:**')
-            .addField("`.nuke`", "Usuwa wszystkie kanały, role oraz banuje użytkowników.")
-            .addField("`.spamroles [nazwa roli]`", "Dodaje bardzo dużą ilość ról.")
-            .addField("`.spamch [nazwa kanału]`", "Dodaje bardzo dużą ilość kanałów tekstowych.")
-            .addField("`.banall`", "Banuje wszystkich członków serwera.")
-            .addField("`.deleteall`", "Usuwa wszystkie kanały oraz kategorie.")
-            .addField("`.roles`", "Wysyła listę roli razem z ich nazwą oraz id")
-            .addField("`.roleall [id roli]`", "Nadaje rolę każdemu na serwerze rolę o podanym id")
-            .setFooter('Made by: Macias#4200 2k19™')
+            .setTitle('**Dsu Nuker:)**')
+            .addField("`.nuke`", "Delete all roles and channel and ban all server members.")
+            .addField("`.spamroles [Name]`", "Create Mass Roles.")
+            .addField("`.spamch [Name]`", "Create Mass Channels.")
+            .addField("`.banall`", "Ban All Server Members.")
+            .addField("`.deleteall`", "Delete all roles and channels.")
+            .addField("`.roles`", "Give You role id ")
+            .addField("`.roleall [role id]`", "Roles Check")
+            .setFooter('Made by: Dsu')
             .setAuthor(message.author.username, message.author.avatarURL)
             .setTimestamp()
-            .setColor('#03fc49');
+            .setColor('RANDOM');
         message.author.send(embed);
         message.delete();
     }
